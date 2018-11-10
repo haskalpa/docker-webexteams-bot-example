f64ddbf6541abc08256773cae77930391fcbca98
Magdi Ayad <magdi.ayad12@gmail.com>
2018-11-10 10:06:09 +0000

    references updated

--- hello_bot/hello_bot.py
+++ hello_bot/hello_bot.py
@@ -3,7 +3,7 @@
 
 # 3rd party imports ------------------------------------------------------------
 from flask import Flask, request
-from ciscosparkapi import CiscoSparkAPI, Webhook
+from webexteamssdk import WebexTeamsAPI, Webhook
 
 # local imports ----------------------------------------------------------------
 from helpers import (read_yaml_data,
@@ -13,11 +13,11 @@ from helpers import (read_yaml_data,
 
 
 flask_app = Flask(__name__)
-spark_api = None
+teams_api = None
 
 
-@flask_app.route('/sparkwebhook', methods=['POST'])
-def sparkwebhook():
+@flask_app.route('/teamswebhook', methods=['POST'])
+def teamswebhook():
     if request.method == 'POST':
 
         json_data = request.json
@@ -28,9 +28,9 @@ def sparkwebhook():
 
         webhook_obj = Webhook(json_data)
         # Details of the message created
-        room = spark_api.rooms.get(webhook_obj.data.roomId)
-        message = spark_api.messages.get(webhook_obj.data.id)
-        person = spark_api.people.get(message.personId)
+        room = teams_api.rooms.get(webhook_obj.data.roomId)
+        message = teams_api.messages.get(webhook_obj.data.id)
+        person = teams_api.people.get(message.personId)
         email = person.emails[0]
 
         print("NEW MESSAGE IN ROOM '{}'".format(room.title))
@@ -39,24 +39,24 @@ def sparkwebhook():
 
         # Message was sent by the bot, do not respond.
         # At the moment there is no way to filter this out, there will be in the future
-        me = spark_api.people.me()
+        me = teams_api.people.me()
         if message.personId == me.id:
             return 'OK'
         else:
-            spark_api.messages.create(room.id, text='hello')
+            teams_api.messages.create(room.id, text='hello, person who has email '+str(email))
     else:
         print('received none post request, not handled!')
 
 
 if __name__ == '__main__':
     config = read_yaml_data('/opt/config/config.yaml')['hello_bot']
-    spark_api = CiscoSparkAPI(access_token=config['spark_access_token'])
+    teams_api = WebexTeamsAPI(access_token=config['teams_access_token'])
 
     ngrok_url = get_ngrok_url()
     webhook_name = 'hello-bot-wb-hook'
-    dev_webhook = find_webhook_by_name(spark_api, webhook_name)
+    dev_webhook = find_webhook_by_name(teams_api, webhook_name)
     if dev_webhook:
-        delete_webhook(spark_api, dev_webhook)
-    create_webhook(spark_api, webhook_name, ngrok_url + '/sparkwebhook')
+        delete_webhook(teams_api, dev_webhook)
+    create_webhook(teams_api, webhook_name, ngrok_url + '/teamswebhook')
 
     flask_app.run(host='0.0.0.0', port=5000)
